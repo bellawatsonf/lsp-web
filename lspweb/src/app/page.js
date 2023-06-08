91cda162778f3a53bf9c12e400eb430b741d0de3
bellawatsonf <bellawatsonf@gmail.com>
2023-06-08 23:36:55 +0700

    add several section

--- lspweb/src/app/page.js
+++ lspweb/src/app/page.js
@@ -1,8 +1,9 @@
 "use client";
 import Image from "next/image";
 import Navbar from "./components/Navbar";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Box } from "@mui/material";
 import styles from "./page.module.css";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
@@ -41,7 +42,7 @@ export default function Home() {
                     padding: "17px 30px",
                     color: "#040924",
                     fontWeight: 700,
-                    marginTop: "20px",
+                    marginTop: "17px",
                     minWidth: "250px",
                     // textTransform: "lowercase !important",
                   }}
@@ -250,6 +251,199 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className={styles["insight-box"]}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            // minHeight: "400px",
+            // height: "100%",
+          }}
+        >
+          <img
+            className={styles["imginsight"]}
+            src="https://www.crayon.co/hs-fs/hubfs/bg-top-pattern.png?width=2718&height=4160&name=bg-top-pattern.png"
+          />
+        </div>
+        <Box sx={{ zIndex: 999, position: "relative", marginTop: "50px" }}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 mb-5">
+                <div className={styles["insight-img"]}>
+                  <img src="https://www.crayon.co/hs-fs/hubfs/ProductImagery_Homepage-01.png?width=550&height=584&name=ProductImagery_Homepage-01.png" />
+                </div>
+              </div>
+              <div className="col-lg-6 mb-5">
+                <div className={styles["desc-insight"]}>
+                  <Typography sx={{ fontSize: "40px" }}>
+                    Way more data,
+                    <br />
+                    Way less time
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#040924",
+                      fontSize: "17px",
+                      paddingTop: "20px",
+                    }}
+                  >
+                    Manual research is not only time-consuming—it’s also risky.
+                    When you rely on human effort, you’re prone to missed
+                    opportunities and undetected threats.
+                    <br />
+                    <br />
+                    With Crayon, you can automatically capture your competitors’
+                    movements in real time. Tap into more than one hundred
+                    different data types across millions of competitive
+                    intelligence sources to stay on top of product updates,
+                    messaging pivots, executive team changes, and more.
+                    <br />
+                    <br />
+                    Learn more about Crayon’s approach to Competitive
+                    Intelligence and What We Track.
+                  </Typography>
+                </div>
+              </div>
+              <div className="col-lg-6 mb-5">
+                <div className={styles["desc-insight"]}>
+                  <Typography sx={{ fontSize: "40px" }}>
+                    Way more data,
+                    <br />
+                    Way less time
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#040924",
+                      fontSize: "17px",
+                      paddingTop: "20px",
+                    }}
+                  >
+                    Manual research is not only time-consuming—it’s also risky.
+                    When you rely on human effort, you’re prone to missed
+                    opportunities and undetected threats.
+                    <br />
+                    <br />
+                    With Crayon, you can automatically capture your competitors’
+                    movements in real time. Tap into more than one hundred
+                    different data types across millions of competitive
+                    intelligence sources to stay on top of product updates,
+                    messaging pivots, executive team changes, and more.
+                    <br />
+                    <br />
+                    Learn more about Crayon’s approach to Competitive
+                    Intelligence and What We Track.
+                  </Typography>
+                </div>
+              </div>
+              <div className="col-lg-6 mb-5">
+                <div className={styles["insight-img"]}>
+                  <img src="https://www.crayon.co/hs-fs/hubfs/ProductImagery_Homepage-01.png?width=550&height=584&name=ProductImagery_Homepage-01.png" />
+                </div>
+              </div>
+
+              <div className="col-lg-6 mb-5">
+                <div className={styles["insight-img"]}>
+                  <img src="https://www.crayon.co/hs-fs/hubfs/ProductImagery_Homepage-01.png?width=550&height=584&name=ProductImagery_Homepage-01.png" />
+                </div>
+              </div>
+              <div className="col-lg-6 mb-5">
+                <div className={styles["desc-insight"]}>
+                  <Typography sx={{ fontSize: "40px" }}>
+                    Way more data,
+                    <br />
+                    Way less time
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#040924",
+                      fontSize: "15px",
+                      paddingTop: "20px",
+                    }}
+                  >
+                    Manual research is not only time-consuming—it’s also risky.
+                    When you rely on human effort, you’re prone to missed
+                    opportunities and undetected threats.
+                    <br />
+                    <br />
+                    With Crayon, you can automatically capture your competitors’
+                    movements in real time. Tap into more than one hundred
+                    different data types across millions of competitive
+                    intelligence sources to stay on top of product updates,
+                    messaging pivots, executive team changes, and more.
+                    <br />
+                    <br />
+                    Learn more about Crayon’s approach to Competitive
+                    Intelligence and What We Track.
+                  </Typography>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Box>
+      </div>
+      <div className={styles["best-product"]}>
+        <Typography
+          sx={{
+            color: "#040924",
+            fontWeight: 500,
+            fontSize: "36px",
+            textAlign: "center",
+            paddingBottom: "50px",
+          }}
+        >
+          Our Best Resources
+        </Typography>
+        <div className="container">
+          <div className="row">
+            <div className="col col-lg-6">
+              <div className={styles["img-bestproduct"]}>
+                <img src="https://www.crayon.co/hs-fs/hubfs/guide-to-ci-cover-page.png?width=284&height=357&name=guide-to-ci-cover-page.png" />
+              </div>
+            </div>
+            <div className="col col-lg-6">
+              <div>
+                <Typography
+                  sx={{
+                    color: "#040924",
+                    fontSize: "36px",
+                    paddingBottom: "20px",
+                  }}
+                >
+                  Ujikompetensi Bla Bla
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "#6F7375;",
+                    fontSize: "17px",
+                    paddingBottom: "20px",
+                  }}
+                >
+                  With a strong competitive intelligence program, you can
+                  provide your marketing, sales, product, and executive teams
+                  with a powerful, strategic advantage. Knowing what your
+                  competitors are up to will help you beat them every time.
+                </Typography>
+              </div>
+              <div className={styles["info-bestproduct"]}>
+                <img
+                  className={styles["imgproduct"]}
+                  src="https://www.crayon.co/hs-fs/hubfs/book-gray.png?width=90&height=90&name=book-gray.png"
+                />
+                <Typography
+                  sx={{
+                    color: "#44474c",
+                    padding: "10px",
+                  }}
+                >
+                  1000 asesi
+                </Typography>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
